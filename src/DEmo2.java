b31046f82e2eaa6a81a7862f28777256663114c9
Shawn Nguon <snguon@student.bridgew.edu>
2020-01-28 10:46:56 -0500

    Creating a program to ask for name and output Hello +name

---
+++ src/DEmo2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class DEmo2 {
+    public static void main(String[] args){
+        var reader = new Scanner(System.in); //want to close most Scanners, NOT System.in
+        System.out.print("What is your name:");
+        var name = reader.nextLine();
+        System.out.println("Hello "+name);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Test line comment
+        var str = "Hi Comp152";
+        System.out.println(str);
+    }
+}
